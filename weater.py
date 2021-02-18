e00ccdddc081adbf50bd099509c598fa1a750dc2
songshanyuwu <songzhijunpro@163.com>
2021-02-18 21:09:46 +0800

    Update weater.py

--- weater.py
+++ weater.py
@@ -6,8 +6,8 @@ import os
 
 
 PUSHPLUSSCKEY = os.environ.get('PUSHPLUSSCKEY') ##PUSHPLUS推送KEY
-SERVERSCKEY = os.environ.get('SERVERSCKEY')                 ##Server酱推送KEY
-COOLSCKEY = os.environ.get('COOLSCKEY')                   ##CoolPush酷推KEY
+SERVERSCKEY = os.environ.get('SERVERSCKEY')     ##Server酱推送KEY
+COOLSCKEY = os.environ.get('COOLSCKEY')         ##CoolPush酷推KEY
 
 
 def get_iciba_everyday():
@@ -16,13 +16,13 @@ def get_iciba_everyday():
     bee = eed.json()  #返回的数据
     english = bee['content']
     zh_CN = bee['note']
-    str = '【奇怪的知识】\n' + english + '\n' + zh_CN
+    str = english + '\n' + zh_CN
     return str
 
 
 def PushPlus(info): #PUSHPLUS酱推送
-    token = PUSHPLUSSCKEY #在pushpush网站中可以找到
-    title= u"天气推送" #改成你要的标题内容
+    token = PUSHPLUSSCKEY               #在pushpush网站中可以找到
+    title= u"天气推送"                   #改成你要的标题内容
     content = info.replace('\n','\n\n') #改成你要的正文内容
     url = 'http://pushplus.hxtrip.com/send'
     data = {
@@ -64,12 +64,36 @@ def main():
         tqurl = api + city_code
         response = requests.get(tqurl)
         d = response.json()         #将数据以json形式返回，这个d就是返回的json数据
+        weather = {}
         if(d['status'] == 200):     #当返回状态码为200，输出天气状况
-            del d['message']
-            del d['status']
-            d["data"] = d["data"]["forecast"][0]
-            # requests.post(cpurl,tdwt.encode('utf-8'))         #把天气数据转换成UTF-8格式，不然要报错。
-            PushPlus(d)
+            # 天气提示内容
+            weather['更新时间'] = d["time"]
+            weather['省市'] = d["cityInfo"]["parent"] + d["cityInfo"]["city"]
+            weather['日期'] = d["data"]["forecast"][0]["ymd"]
+            weather['星期'] = d["data"]["forecast"][0]["week"]
+            weather['天气'] = d["data"]["forecast"][0]["type"]
+            weather['温度'] = d["data"]["forecast"][0]["high"] + d["data"]["forecast"][0]["low"]
+            weather['PM2.5'] = str(d["data"]["pm25"])
+            weather['PM10'] = str(d["data"]["pm10"])
+            weather['天气质量'] = d["data"]["quality"]
+            weather['风力风向'] = d["data"]["forecast"][0]["fx"] + d["data"]["forecast"][0]["fl"]
+            weather['感冒指数'] = d["data"]["ganmao"]
+            weather['温馨提示'] = d["data"]["forecast"][0]["notice"]
+            weather['每日英语句子'] = get_iciba_everyday()
+
+            # requests.post(cpurl,tdwt.encode('utf-8')) #把天气数据转换成UTF-8格式，不然要报错。
+            
+            PushPlus(str(weather))
+            
+            # 天气提示内容
+            tdwt = "【今日份天气】\n城市： " + d["cityInfo"]["parent"] + d["cityInfo"]["city"] + \
+                   "\n日期： " + d["data"]["forecast"][0]["ymd"] + "\n星期: " + d["data"]["forecast"][0]["week"] + \
+                   "\n天气: " + d["data"]["forecast"][0]["type"] + "\n温度: " + d["data"]["forecast"][0]["high"] + " / "+ d["data"]["forecast"][0]["low"] + \
+                   "\n湿度: " + d["data"]["shidu"] + "\nPM25: " + str(d["data"]["pm25"]) + "\nPM10: " + str(d["data"]["pm10"]) + \
+                   "\n空气质量: " + d["data"]["quality"] + "\n风力风向: " + d["data"]["forecast"][0]["fx"] + d["data"]["forecast"][0]["fl"] + \
+                   "\n感冒指数: "  + d["data"]["ganmao"] + "\n温馨提示： " + d["data"]["forecast"][0]["notice"] + "\n更新时间: " + d["time"] + \
+                   "\n✁-----------------------------------------\n" + get_iciba_everyday()
+            # print(tdwt)
             ServerPush(tdwt)
             CoolPush(tdwt)
     except Exception:
