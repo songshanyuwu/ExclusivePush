d4dbd0af7cdbd8eb1aa3f4f3737538d788b6a450
songshanyuwu <songzhijunpro@163.com>
2021-02-18 19:07:10 +0800

    Update weater.py

--- weater.py
+++ weater.py
@@ -65,28 +65,6 @@ def main():
         response = requests.get(tqurl)
         d = response.json()         #将数据以json形式返回，这个d就是返回的json数据
         if(d['status'] == 200):     #当返回状态码为200，输出天气状况
-            parent = d["cityInfo"]["parent"] #省
-            city = d["cityInfo"]["city"] #市
-            update_time = d["time"] #更新时间
-            date = d["data"]["forecast"][0]["ymd"] #日期
-            week = d["data"]["forecast"][0]["week"] #星期
-            weather_type = d["data"]["forecast"][0]["type"] # 天气
-            wendu_high = d["data"]["forecast"][0]["high"] #最高温度
-            wendu_low = d["data"]["forecast"][0]["low"] #最低温度
-            shidu = d["data"]["shidu"] #湿度
-            pm25 = str(d["data"]["pm25"]) #PM2.5
-            pm10 = str(d["data"]["pm10"]) #PM10
-            quality = d["data"]["quality"] #天气质量
-            fx = d["data"]["forecast"][0]["fx"] #风向
-            fl = d["data"]["forecast"][0]["fl"] #风力
-            ganmao = d["data"]["ganmao"] #感冒指数
-            tips = d["data"]["forecast"][0]["notice"] #温馨提示
-            # 天气提示内容
-            tdwt = "【今日份天气】\n城市： " + parent + city + \
-                   "\n日期： " + date + "\n星期: " + week + "\n天气: " + weather_type + "\n温度: " + wendu_high + " / "+ wendu_low + "\n湿度: " + \
-                    shidu + "\nPM25: " + pm25 + "\nPM10: " + pm10 + "\n空气质量: " + quality + \
-                   "\n风力风向: " + fx + fl + "\n感冒指数: "  + ganmao + "\n温馨提示： " + tips + "\n更新时间: " + update_time + "\n✁-----------------------------------------\n" + get_iciba_everyday()
-            
             del d['message']
             del d['status']
             d["data"] = d["data"]["forecast"][0]
