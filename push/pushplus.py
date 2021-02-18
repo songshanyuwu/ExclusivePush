ba29b499e89976c383ab9e0f8359ed03ec3b7bb7
songshanyuwu <songzhijunpro@163.com>
2021-02-18 17:04:40 +0800

    Update pushplus.py

--- push/pushplus.py
+++ push/pushplus.py
@@ -2,8 +2,8 @@
 import requests
 import json
 token = '49bff5f3bc2f4d1f8415d138ce3e5b51' #在pushpush网站中可以找到
-title= '测试标题' #改成你要的标题内容
-content ='我的内容' #改成你要的正文内容
+title= '祝宋金涛开心快乐成长' #改成你要的标题内容
+content ='O(∩_∩)O哈哈~' #改成你要的正文内容
 url = 'http://pushplus.hxtrip.com/send'
 data = {
     "token":token,
