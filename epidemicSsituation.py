bed4e084e7c75bdc054e822bb17bfe9f8200ceef
songshanyuwu <songzhijunpro@163.com>
2021-06-01 11:46:49 +0800

    Update epidemicSsituation.py

--- epidemicSsituation.py
+++ epidemicSsituation.py
@@ -12,6 +12,7 @@ coolpush_key = os.environ.get('COOLSCKEY')         ##CoolPush酷推KEY
 qmsg_key = os.environ.get('QMSGSCKEY')         ##CoolPush酷推KEY
 
 
+
 def UserAgent(): #随机获取请求头
     user_agent_list = ['Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1464.0 Safari/537.36',
                    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36',
@@ -48,13 +49,13 @@ def get_data(url_json):
     lastUpdateTime = url_json['data']['lastUpdateTime']#截止时间
 
     data ='[+] 标题：' + '全国疫情数据实时统计' + '\n\n' + \
-          '统计截至时间：'+ lastUpdateTime +'\n\n' + \
-          '累计确诊：' + total_confirm + ' ; ' + '较昨日：' + today_confirm +'\n\n' + \
-          '现有确诊：' + total_storeConfirm + ' ; ' + '较昨日：' + today_storeConfirm +'\n\n' + \
-          '累计死亡：' + total_dead + ' ; ' + '较昨日：' + today_dead +'\n\n' + \
-          '累计治愈：' + total_heal + ' ; ' + '较昨日：' + today_heal +'\n\n' + \
-          '境外输入：' + total_input + ' ; ' + '较昨日：' + today_input +'\n\n' + \
-          '无症状感染者：' + total_noSymptom + ' ; ' + '较昨日：' + today_incrNoSymptom +'\n\n' + \
+          '统计截至时间：'+ lastUpdateTime +'\n' + \
+          '累计确诊：' + total_confirm + ' ; ' + '较昨日：' + today_confirm +'\n' + \
+          '现有确诊：' + total_storeConfirm + ' ; ' + '较昨日：' + today_storeConfirm +'\n' + \
+          '累计死亡：' + total_dead + ' ; ' + '较昨日：' + today_dead +'\n' + \
+          '累计治愈：' + total_heal + ' ; ' + '较昨日：' + today_heal +'\n' + \
+          '境外输入：' + total_input + ' ; ' + '较昨日：' + today_input +'\n' + \
+          '无症状感染者：' + total_noSymptom + ' ; ' + '较昨日：' + today_incrNoSymptom +'\n' + \
           "\n✁-----------------------------------------\n"
     return data
 
@@ -77,11 +78,11 @@ def get_data2(url_json):
     lastUpdateTime = url_json['lastUpdateTime']#截止时间
 
     data ='[+] 标题：' + url_json['name'] + '疫情数据实时统计' + '\n\n' + \
-          '统计截至时间：'+ lastUpdateTime + '\n\n' + \
-          '累计确诊：' + total_confirm + ' ; ' + '较昨日：' + today_confirm + '\n\n' + \
-          '现有确诊：' + total_storeConfirm + ' ; ' + '较昨日：' + today_storeConfirm + '\n\n' + \
-          '累计死亡：' + total_dead + ' ; ' + '较昨日：' + today_dead + '\n\n' + \
-          '累计治愈：' + total_heal + ' ; ' + '较昨日：' + today_heal + '\n\n' + \
+          '统计截至时间：'+ lastUpdateTime + '\n' + \
+          '累计确诊：' + total_confirm + ' ; ' + '较昨日：' + today_confirm + '\n' + \
+          '现有确诊：' + total_storeConfirm + ' ; ' + '较昨日：' + today_storeConfirm + '\n' + \
+          '累计死亡：' + total_dead + ' ; ' + '较昨日：' + today_dead + '\n' + \
+          '累计治愈：' + total_heal + ' ; ' + '较昨日：' + today_heal + '\n' + \
           "\n✁-----------------------------------------\n"
     return data
 
@@ -98,56 +99,52 @@ def Get_Url():
             for l2 in line['children'] :
                 if l2['name'] in ['北京','山东']:
                     if l2['name'] == '北京':
-                        data2 = data2 + '\n\n' + get_data2(l2)
+                        data2 = data2 + '\n' + get_data2(l2)
                     for l3 in l2['children']:
                         if l3['name'] in ['济南','东营','烟台']:
-                            print(l3)
-                            data2 = data2 + '\n\n'  + get_data2(l3)
+                            # print(l3)
+                            data2 = data2 + '\n'  + get_data2(l3)
 
-    data = data + data2
-
-    select_robots(0,data) #3为Qmsg推送，1为酷推推送，2为server酱推送。默认为0
+    pneumoniaData = data + data2
+    print(pneumoniaData)
+    select_robots(0,pneumoniaData) #3为Qmsg推送，1为酷推推送，2为server酱推送。默认为0
     print('ok')
 
 
-def select_robots(i,data):
+def select_robots(i,pneumoniaData):
     if i == 0:
-        HtmlPuch_PushPlus(data)
+        HtmlPuch_PushPlus(pneumoniaData)
     elif i == 1:
-        HtmlPuch_coolpush(data)
+        HtmlPuch_coolpush(pneumoniaData)
     elif i == 2:
-        HtmlPuch_server(data)
+        HtmlPuch_server(pneumoniaData)
     elif i == 3:
-        HtmlPuch_Qmsg(data)
+        HtmlPuch_Qmsg(pneumoniaData)
     else:
         print('选择错误!')
 
 
-def HtmlPuch_server(data): #server酱推送
+def HtmlPuch_server(pneumoniaData): #server酱推送
     url_key = "https://sc.ftqq.com/" + server_key + ".send"
-    push_data = {'text':"全国疫情数据实时统计",'desp':data}
+    push_data = {'text':"全国疫情数据实时统计",'desp':pneumoniaData}
     html = requests.post(url_key,headers=UserAgent(),data=push_data)
 
-def HtmlPuch_coolpush(data):  #酷推推送
+def HtmlPuch_coolpush(pneumoniaData):  #酷推推送
     url_key = "https://push.xuthus.cc/send/" + coolpush_key
-    push_data = {'c':data}
+    push_data = {'c':pneumoniaData}
     html = requests.get(url=url_key,params=push_data,headers=UserAgent())
 
-def HtmlPuch_Qmsg(data):  #Qmsg推送
+def HtmlPuch_Qmsg(pneumoniaData):  #Qmsg推送
     url_key = "https://qmsg.zendee.cn/send/" + qmsg_key
-    push_data = {'msg':data}
+    push_data = {'msg':pneumoniaData}
     html = requests.get(url=url_key,params=push_data,headers=UserAgent())
 
-def HtmlPuch_PushPlus(data): #PushPlus推送
-    url = 'http://pushplus.hxtrip.com/send'
-    data = {
-        "token":pushplus_key,
-        "title":u"全国疫情数据实时统计",
-        "content":data
-    }
-    body=json.dumps(data).encode(encoding='utf-8')
-    headers = {'Content-Type':'application/json'}
-    requests.post(url, data=body, headers=headers)
+def HtmlPuch_PushPlus(pneumoniaData): #PushPlus推送
+    token = pushplus_key #在pushplus网站中可以找到
+    title= '全国疫情数据实时统计' #改成你要的标题内容
+    content = pneumoniaData #改成你要的正文内容
+    url = 'http://www.pushplus.plus/send?token='+token+'&title='+title+'&content='+content+'&template=html'
+    requests.get(url)
 
 
 if __name__ == '__main__':
