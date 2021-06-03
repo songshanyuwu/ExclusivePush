a718ac986254ca45747a30fe2bb2c16275f7f6bf
songshanyuwu <songzhijunpro@163.com>
2021-06-03 09:25:47 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -5,17 +5,10 @@ import requests, json
 import os
 
 
-# PUSHPLUSSCKEY = os.environ.get('PUSHPLUSSCKEY') ##PUSHPLUS推送KEY
-# SERVERSCKEY = os.environ.get('SERVERSCKEY')     ##Server酱推送KEY
-# COOLSCKEY = os.environ.get('COOLSCKEY')         ##CoolPush酷推KEY
+PUSHPLUSSCKEY = os.environ.get('PUSHPLUSSCKEY') ##PUSHPLUS推送KEY
+SERVERSCKEY = os.environ.get('SERVERSCKEY')     ##Server酱推送KEY
+COOLSCKEY = os.environ.get('COOLSCKEY')         ##CoolPush酷推KEY
 
-
-def HtmlPuch_PushPlus(pneumoniaData): #PushPlus推送
-    token = pushplus_key #在pushplus网站中可以找到
-    title= '全国疫情数据实时统计' #改成你要的标题内容
-    content = pneumoniaData #改成你要的正文内容
-    url = 'http://www.pushplus.plus/send?token='+token+'&title='+title+'&content='+content+'&template=html'
-    requests.get(url)
 
 def PushPlus(info): #PUSHPLUS酱推送
     token = PUSHPLUSSCKEY               #在pushpush网站中可以找到
